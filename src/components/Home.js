2ae1b16e9ef92daa265c56361b603b9910cd5cc3
jessicagomesng <jgnpeluso@gmail.com>
2020-12-17 16:13:43 +0000

    refactored Home as functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,60 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
 import logo from '../css/images/logo.png';
 
-class Home extends React.Component {
-
-    handleLogOut = () => {
-        axios.post('http://localhost:3001/logout', {withCredentials: true})
-        .then(response => {
-          sessionStorage.removeItem('jwt');
-          console.log(response);
-          this.props.logOut(response.data);
-          console.log(this.props)
-          this.props.history.push('/')
-        })
-      }
-
-    render() {
-        // if (this.props.isLoggedIn === 'true') {
-        //     return (
-        //         <>
-        //             <Link to="/map">Map</Link>
-        //             <Link to="/logout">Log Out</Link>
-        //         </>
-        //     )
-        // } else {
-        return (
-            <>
-            <img src={logo} alt="Places I've Been logo" id="main-logo" />
-            <Link to="/signup">Sign Up</Link> 
-            <Link to="/login">Log In</Link>
-            </>
-        )
-        //     {/* return (
-        //         <>
-        //         <img src={logo} alt="Places I've Been logo"/>
-        //             <Link to="/signup">Sign Up</Link>
-        //             <Link to="/login">Log In</Link>
-        //             <Link to="/logout" onClick={this.handleLogOut}>Log Out</Link>
-        //             <Link to="/pins">View Pins</Link>
-        //             <Link to="/add-a-pin">Add A Pin</Link>
-        //         </>
-        //     )
-        // // } */}
-    }
+const Home = (props) => {
+  return (
+    <>
+      <img src={logo} alt="Places I've Been logo" id="main-logo" />
+      <Link to="/signup">Sign Up</Link> 
+      <Link to="/login">Log In</Link>
+    </>
+  )
 }
 
-// const Home = () => {
-//     const history = useHistory();
-//     return (
-//         <>
-//             <button onClick={ () => history.push('/map') } >Map</button>
-//         </>
-//     )
-    
-// }
-
-export default withRouter(Home);
+export default Home;
